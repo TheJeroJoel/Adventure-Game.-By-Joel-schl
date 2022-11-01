5122565c8573773707c84376b2e4714be1c474a3
Joel Schlueter <joelschl787@ed.amdsb.ca>
2022-11-01 10:22:07 -0400

    Add project files.

---
+++ Adventure Game. By Joel schl/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Adventure_Game.By_Joel_schl
+{
+    partial class AdventureGameForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdventureGameForm));
+            this.Option1Button = new System.Windows.Forms.Button();
+            this.Option2Button = new System.Windows.Forms.Button();
+            this.GameNameLabel = new System.Windows.Forms.Label();
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.ImageLabel = new System.Windows.Forms.Label();
+            this.Option2Label = new System.Windows.Forms.Label();
+            this.Option1Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Option1Button
+            // 
+            this.Option1Button.Location = new System.Drawing.Point(12, 263);
+            this.Option1Button.Name = "Option1Button";
+            this.Option1Button.Size = new System.Drawing.Size(90, 35);
+            this.Option1Button.TabIndex = 0;
+            this.Option1Button.Text = "Option 1";
+            this.Option1Button.UseVisualStyleBackColor = true;
+            this.Option1Button.Click += new System.EventHandler(this.Option1Button_Click);
+            // 
+            // Option2Button
+            // 
+            this.Option2Button.Location = new System.Drawing.Point(12, 320);
+            this.Option2Button.Name = "Option2Button";
+            this.Option2Button.Size = new System.Drawing.Size(90, 35);
+            this.Option2Button.TabIndex = 1;
+            this.Option2Button.Text = "Option 2";
+            this.Option2Button.UseVisualStyleBackColor = true;
+            this.Option2Button.Click += new System.EventHandler(this.Option2Button_Click);
+            // 
+            // GameNameLabel
+            // 
+            this.GameNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GameNameLabel.Location = new System.Drawing.Point(135, 9);
+            this.GameNameLabel.Name = "GameNameLabel";
+            this.GameNameLabel.Size = new System.Drawing.Size(277, 34);
+            this.GameNameLabel.TabIndex = 2;
+            this.GameNameLabel.Text = "Slime Adventure game";
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.Location = new System.Drawing.Point(12, 225);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(534, 35);
+            this.OutputLabel.TabIndex = 14;
+            this.OutputLabel.Text = "You are a green slime going to the local convience stores";
+            // 
+            // ImageLabel
+            // 
+            this.ImageLabel.Location = new System.Drawing.Point(32, 52);
+            this.ImageLabel.Name = "ImageLabel";
+            this.ImageLabel.Size = new System.Drawing.Size(495, 155);
+            this.ImageLabel.TabIndex = 13;
+            this.ImageLabel.Text = "ImageLabel";
+            // 
+            // Option2Label
+            // 
+            this.Option2Label.AutoSize = true;
+            this.Option2Label.Location = new System.Drawing.Point(108, 340);
+            this.Option2Label.Name = "Option2Label";
+            this.Option2Label.Size = new System.Drawing.Size(98, 13);
+            this.Option2Label.TabIndex = 12;
+            this.Option2Label.Text = "Go to the 2nd store";
+            // 
+            // Option1Label
+            // 
+            this.Option1Label.AutoSize = true;
+            this.Option1Label.Location = new System.Drawing.Point(108, 283);
+            this.Option1Label.Name = "Option1Label";
+            this.Option1Label.Size = new System.Drawing.Size(94, 13);
+            this.Option1Label.TabIndex = 11;
+            this.Option1Label.Text = "Go to the 1st store";
+            // 
+            // AdventureGameForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 383);
+            this.Controls.Add(this.OutputLabel);
+            this.Controls.Add(this.ImageLabel);
+            this.Controls.Add(this.Option2Label);
+            this.Controls.Add(this.Option1Label);
+            this.Controls.Add(this.GameNameLabel);
+            this.Controls.Add(this.Option2Button);
+            this.Controls.Add(this.Option1Button);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AdventureGameForm";
+            this.Text = "Adventure game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Option1Button;
+        private System.Windows.Forms.Button Option2Button;
+        private System.Windows.Forms.Label GameNameLabel;
+        private System.Windows.Forms.Label OutputLabel;
+        private System.Windows.Forms.Label ImageLabel;
+        private System.Windows.Forms.Label Option2Label;
+        private System.Windows.Forms.Label Option1Label;
+    }
+}
+
---
+++ Adventure Game. By Joel schl/Form1.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Adventure_Game.By_Joel_schl
+{
+    public partial class AdventureGameForm : Form
+    {
+        int page = 1;
+        public AdventureGameForm()
+        {
+            InitializeComponent();
+            // QuestionButton.Enabled = false;
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            //StartButtonClickLabel.Text = ".";
+            //StartButtonClickLabel.Enabled = false;
+            //StartButton.Enabled = false;
+            //QuestionButton.Enabled = true;
+        }
+
+        private void Option1Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 7;
+
+            }
+            else if (page == 2)
+            {
+                page = 2;
+            }
+            else if (page == 3)
+            {
+                page = 3;
+            }
+            else if (page == 4)
+            {
+                page = 4;
+            }
+            else if (page == 5)
+            {
+                page = 5;
+            }
+            else if (page == 6)
+            {
+                page = 6;
+            }
+            else if (page == 7)
+            {
+                page = 16;
+            }
+            else if (page == 8)
+            {
+                
+            }
+            else if (page == 9)
+            {
+                page = 9;
+            }
+            else if (page == 10)
+            {
+                page = 10;
+            }
+            else if (page == 11)
+            {
+                page = 11;
+            }
+            else if (page == 12)
+            {
+                page = 12;
+            }
+            else if (page == 13)
+            {
+                page = 13;
+            }
+            else if (page == 14)
+            {
+                page = 14;
+            }
+            else if (page == 15)
+            {
+                page = 15;
+            }
+            else if (page == 16)
+            {
+                page = 18;
+            }
+            else if (page == 17)
+            {
+                page = 17;
+            }
+            else if (page == 18)
+            {
+                page = 19;
+            }
+            else if (page == 19)
+            {
+                page = 20;
+            }
+            else if (page == 20)
+            {
+                page = 1;
+            }
+            else if (page == 21)
+            {
+                page = 1;
+            }
+            switch (page)
+            {
+                case 1:
+                    OutputLabel.Text = "You are a green slime going to the local convience stores nearby";
+                    Option1Label.Text = "Go to the 1st store";
+                    Option2Label.Text = "Go to the 2nd store";
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    OutputLabel.Text = "You slide infront of the 1st store";
+                    Option1Label.Text = "Do you slide underneath the door?";
+                    Option2Label.Text = "Do you wait for someone to open the door?";
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    break;
+                case 12:
+                    break;
+                case 13:
+                    break;
+                case 14:
+                    break;
+                case 15:
+                    break;
+                case 16:
+                    OutputLabel.Text = "You try sliding underneath but the bottom of the door is the most foul disguisting place you have ever seen, you slide underneath and become filthy";
+                    Option1Label.Text = "be dirty";
+                    Option2Label.Text = "Don't be dirty";
+                    break;
+                case 17:
+                    break;
+                case 18:
+                    OutputLabel.Text = "Your slimy geletin of a body starts to break into small hardened chunks and you dry up. (until the next rainy day)";
+                    Option1Label.Text = "Click me";
+                    Option2Label.Text = "_";
+                    break;
+                case 19:
+                    OutputLabel.Text = "Play Again?";
+                    Option1Label.Text = "Yes";
+                    Option2Label.Text = "No";
+                    break;
+                case 20:
+                    OutputLabel.Text = "Ok";
+                    Option1Label.Text = "Click me Again";
+                    Option2Label.Text = "Changed my mind (Closes Game)";
+                    break;
+                case 21:
+                    OutputLabel.Text = "You stand there as more slimes and regular everyday humans enter the store stepping over you trying not to make a mess";
+                    Option1Label.Text = "Keep Waiting";
+                    Option2Label.Text = "Leave";
+                    break;
+                
+                
+            }
+        }
+
+        private void Option2Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 1;
+
+            }
+            else if (page == 2)
+            {
+                page = 2;
+            }
+            else if (page == 3)
+            {
+                page = 3;
+            }
+            else if (page == 4)
+            {
+                page = 4;
+            }
+            else if (page == 5)
+            {
+                page = 5;
+            }
+            else if (page == 6)
+            {
+                page = 6;
+            }
+            else if (page == 7)
+            {
+                Random random = new Random();
+                int chance = random.Next(1, 101);
+                if (chance < 60)
+                {
+                    page = 8;
+                }
+                else
+                {
+                    page = 21;
+                }
+            }
+            else if (page == 8)
+            {
+                page = 8;
+            }
+            else if (page == 9)
+            {
+                page = 9;
+            }
+            else if (page == 10)
+            {
+                page = 10;
+            }
+            else if (page == 11)
+            {
+                page = 11;
+            }
+            else if (page == 12)
+            {
+                page = 12;
+            }
+            else if (page == 13)
+            {
+                page = 13;
+            }
+            else if (page == 14)
+            {
+                page = 14;
+            }
+            else if (page == 15)
+            {
+                page = 15;
+            }
+            else if (page == 16)
+            {
+                page = 16;
+            }
+            else if (page == 17)
+            {
+                page = 17;
+            }
+            else if (page == 18)
+            {
+                page = 18;
+            }
+            else if (page == 19)
+            {
+                page = 20;
+            }
+            else if (page == 20)
+            {
+                page = 1;
+            }
+            else if (page == 21)
+            {
+                page = 0;
+            }
+           
+            switch (page)
+            {
+                case 1:
+                    OutputLabel.Text = "You go to the 2nd store which has its door hung open";
+                    Option1Label.Text = "Talk to the store clerk";
+                    Option2Label.Text = "Don't talk to the store clerk";
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    break;
+                case 12:
+                    break;
+                case 13:
+                    break;
+                case 14:
+                    break;
+                case 15:
+                    break;
+                case 16:
+                    break;
+                case 17:
+                    break;
+                case 18:
+                    break;
+                case 19:
+
+                    break;
+                case 20:
+                    OutputLabel.Text = "Ok Bye";
+                    Refresh();
+                    Thread.Sleep(2000);
+                    Application.Exit();
+                    break;
+                case 21:
+
+                    break;
+                
+            }
+        }
+    }
+}
